755ca8e5e1f9241bc75a8400d1da06d3aa36053d
Vanshika Rajput <vanshikaacodes@gmail.com>
2026-06-30 23:42:19 +0530

    Implement health route and homepage with timestamp

--- guardian-backend/src/app.js
+++ guardian-backend/src/app.js
@@ -3,18 +3,24 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-app.use(express.json());// this allows req.body (auth-control)
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
+// Health Check Route
+app.get("/health", (req, res) => {
+  res.send(`
+    <h2>GUARDIAN API IS HEALTHY</h2>
+    <p>AUTHOR : VANSHIKA RAJPUT</p>
+  `);
+});
+
+// Home Route
 app.get("/", (req, res) => {
-
-  res.json({
-
-    message: "Guardian backend is running 🚀",
-
-  });
-  
+  res.send(`
+    <h2>Guardian backend is running 🚀</h2>
+    <h3>Time: ${new Date().toLocaleString()}</h3>
+  `);
 });
 
 module.exports = app;
