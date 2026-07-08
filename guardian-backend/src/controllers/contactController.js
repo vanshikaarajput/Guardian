eae049480009ff01570c3b5937790b7e019d30f0
Vanshika Rajput <vanshikaacodes@gmail.com>
2026-07-08 19:18:59 +0530

    connection to route

--- guardian-backend/src/controllers/contactController.js
+++ guardian-backend/src/controllers/contactController.js
@@ -1,23 +1,21 @@
-const { createContact } = require("../services/contactService");
+const {
+  createContact,
+  getContacts,
+} = require("../services/contactService");
 
 const addContact = async (req, res) => {
   try {
-
-    //we rcv data from postman
+    // Receive data from Postman
     const { name, phone, relation } = req.body;
 
+    // Validate input
     if (!name || !phone || !relation) {
-
-        //validate input
       return res.status(400).json({
         message: "Please provide all required fields",
       });
     }
 
-
-    //calls the service : controller doesnot talk to prisma directly 
-    //rather follows a clean arch - route controller - service - prisma - db
-
+    // Create the contact
     const contact = await createContact(req.user.userId, {
       name,
       phone,
@@ -37,6 +35,25 @@ const addContact = async (req, res) => {
   }
 };
 
+const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await getContacts(req.user.userId);
+
+    return res.status(200).json({
+      success: true,
+      count: contacts.length,
+      contacts,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
 module.exports = {
   addContact,
+  getAllContacts,
 };
--- guardian-backend/src/routes/contactRoutes.js
+++ guardian-backend/src/routes/contactRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { addContact } = require("../controllers/contactController");
+const {
+  addContact,
+  getAllContacts,
+} = require("../controllers/contactController");
+
 const protect = require("../middleware/authMiddleware");
 
 // Add a new emergency contact
 router.post("/", protect, addContact);
+
+// Get all emergency contacts of logged-in user
+router.get("/", protect, getAllContacts);
 
 module.exports = router;
